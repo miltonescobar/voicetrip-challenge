baef05205e094afb7e733768ed76a3dfc9250edc
Milton Escobar <milton.escobar1@gmail.com>
2019-07-08 17:17:25 -0400

    feat: create minimal Map.vue component
    create minimal Map.vue component and insert it into App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+  <div>
+    <Map msg="Hello Map!" />
   </div>
 </template>
+
+<script>
+import Map from "./components/Map.vue";
+export default {
+  name: "App",
+  components: {
+    Map
+  }
+};
+</script>
 
 <style lang="scss">
 #app {
---
+++ src/components/Map.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>{{msg}}</h1>
+</template>
+
+<script>
+export default {
+  name: "Map",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style></style>

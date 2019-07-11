39c8a6af5632dbad90a99cebc9118e5afde1bc20
Milton Escobar <milton.escobar1@gmail.com>
2019-07-11 11:05:12 -0400

    feat: add cors-anywhere third party proxy
    Avoid error because of browser CORS security policy

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>{{msg}}</h1>
+  <div id="map"></div>
 </template>
 
 <script>
@@ -7,8 +7,24 @@ export default {
   name: "Map",
   props: {
     msg: String
+  },
+  data() {
+    return {
+      url:
+        "https://maps.googleapis.com/maps/api/js?key=AIzaSyCZZPJ-EA5cmVovoldFqCG2TX6CqSmNW5c&callback=initMap"
+    };
+  },
+  mounted() {
+    const proxyurl = "https://cors-anywhere.herokuapp.com/";
+    //const url = "https://example.com"; // site that doesn’t send Access-Control-*
+    fetch(proxyurl + this.url) // https://cors-anywhere.herokuapp.com/https://example.com
+    .then(response => response.text())
+    .then(contents => {
+      console.log(contents);
+
+    })
+    .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
   }
 };
 </script>
-
 <style></style>

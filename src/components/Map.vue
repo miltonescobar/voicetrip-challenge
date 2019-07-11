b5c96916cdc7e2a6d6868d52e2e9d72833ed229a
Milton Escobar <milton.escobar1@gmail.com>
2019-07-11 11:15:40 -0400

    style: prettify Map.vue

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -18,12 +18,15 @@ export default {
     const proxyurl = "https://cors-anywhere.herokuapp.com/";
     //const url = "https://example.com"; // site that doesn’t send Access-Control-*
     fetch(proxyurl + this.url) // https://cors-anywhere.herokuapp.com/https://example.com
-    .then(response => response.text())
-    .then(contents => {
-      console.log(contents);
-
-    })
-    .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
+      .then(response => response.text())
+      .then(contents => {
+        console.log(contents);
+      })
+      .catch(() =>
+        console.log(
+          "Can’t access " + this.url + " response. Blocked by browser?"
+        )
+      );
   }
 };
 </script>

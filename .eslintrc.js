bf0b1430432f1262254f8154c1b0c324f5c19a2b
Milton Escobar <milton.escobar1@gmail.com>
2019-07-13 02:01:27 -0400

    build: turn off ESlint rules to avoid contradicting complaints
    turn off ESlint rules to avoid contradicting complaints regarding the closing brackets in template anchor (a) tags

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,8 @@ module.exports = {
   ],
   // add your custom rules here
   rules: {
-    'nuxt/no-cjs-in-config': 'off'
+    'nuxt/no-cjs-in-config': 'off',
+    'vue/html-self-closing': 'off',
+    'prettier/prettier': 'off'
   }
 }

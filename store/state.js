6e341dee9731da39d42ac924786db0f74bb1de25
Milton Escobar <milton.escobar1@gmail.com>
2019-07-16 19:35:52 -0400

    feat: setup the state resolves #13

---
+++ store/state.js
@@ -0,0 +1,12 @@
+export default {
+  products: [],
+  position: {
+    lat: 45.54349803934773,
+    lng: -73.54203346804981
+  },
+  radius: 5,
+  searchTerm: '',
+  price: 10,
+  activeStore: null,
+  locations: []
+};
